7e30bce134dac3614a38c960fd01bad339c77a34
yamatakau08 <yamatakau08@gmail.com>
2026-09-10 13:19:20 +0900

    fix(flake): switch nixpkgs input to nixos-unstable
    Update nixpkgs URL from nixos-25.05 to nixos-unstable to keep yt-dlp and other tools up to date and suppress outdated package warnings.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake settings for Nix develop, dualsubgen";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }:
